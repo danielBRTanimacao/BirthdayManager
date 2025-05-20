34c898415ae6441eb5e104fd130e27df1977a123
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-05-20 13:05:52 -0300

    feat: created CORS basic verification and a fetch api on front end

--- backend/src/main/java/com/daniel/backend/controllers/BirthdayController.java
+++ backend/src/main/java/com/daniel/backend/controllers/BirthdayController.java
@@ -4,7 +4,6 @@ import com.daniel.backend.entities.BirthdayEntity;
 import com.daniel.backend.repositories.BirthdayRepository;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;

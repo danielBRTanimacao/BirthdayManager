aa22c796a4cd32ac4be0495d64584ea7c7372ef3
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-06-01 19:08:15 -0300

    removing some anotations

--- backend/src/main/java/com/daniel/backend/exceptions/ValidationExceptionHandler.java
+++ backend/src/main/java/com/daniel/backend/exceptions/ValidationExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.daniel.backend.exceptions;
 
 import org.postgresql.util.PSQLException;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;

6efc5510bd4b5df2a925d0568c1e63c1c93a1b84
danielBRTanimacao <danieltenorio2046@gmail.com>
2025-06-15 21:03:47 -0300

    feat: refactoring angular front

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './pages/main/main.component';
-import { AuthPageComponent } from './pages/auth-page/auth-page.component';
 import { AuthGuard } from './utils/app.guard';
 import { NgModule } from '@angular/core';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { NoAuthGuard } from './utils/no-auth.guard';
+import { AuthComponent } from './pages/auth/auth.component';
 
 export const routes: Routes = [
     { path: '', component: MainComponent, canActivate: [AuthGuard] },
-    { path: 'auth', component: AuthPageComponent, canActivate: [NoAuthGuard] },
+    { path: 'auth', component: AuthComponent, canActivate: [NoAuthGuard] },
     { path: '**', component: PageNotFoundComponent },
 ];
 
--- frontend/src/app/pages/main/main.component.ts
+++ frontend/src/app/pages/main/main.component.ts
@@ -1,45 +1,56 @@
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
-import { ModalFormComponent } from '../../components/modal-form/modal-form.component';
-import { ModalConfigComponent } from '../../components/modal-config/modal-config.component';
 import { PostItComponent } from '../../components/post-it/post-it.component';
 import { Router } from '@angular/router';
-import { ModalEditableComponent } from '../../components/modal-editable/modal-editable.component';
+import { ModalBaseComponent } from '../../components/modal-base/modal-base.component';
+import { BirthdayService } from '../../services/BirthdayServices';
+import {
+    FormControl,
+    FormGroup,
+    ReactiveFormsModule,
+    Validators,
+} from '@angular/forms';
 
 @Component({
     selector: 'app-main',
-    imports: [
-        ModalFormComponent,
-        ModalConfigComponent,
-        PostItComponent,
-        ModalEditableComponent,
-    ],
+    imports: [ModalBaseComponent, PostItComponent, ReactiveFormsModule],
     templateUrl: './main.component.html',
     styleUrl: './main.component.css',
 })
 export class MainComponent {
     httpClient = inject(HttpClient);
     router = inject(Router);
+    birthdayService = inject(BirthdayService);
     title = 'Lembrete aniversarios';
-    dados: any;
+    data: any;
+
+    form = new FormGroup({
+        userId: new FormControl({
+            value: localStorage.getItem('userId'),
+            disabled: false,
+        }),
+        name: new FormControl('', Validators.required),
+        birthday: new FormControl('', [Validators.required]),
+        notes: new FormControl(''),
+        colors: new FormControl(''),
+        textColor: new FormControl(''),
+    });
 
     constructor() {
-        const token = localStorage.getItem('token');
-
-        const headers = new HttpHeaders({
-            Authorization: `Bearer ${token}`,
+        this.birthdayService.getBirthdays().subscribe({
+            next: (res) => {
+                this.data = res;
+                console.log('Aniversários:', res);
+            },
+            error: (err) => {
+                localStorage.removeItem('token');
+                console.error(err);
+            },
         });
+    }
 
-        this.httpClient
-            .get('http://127.0.0.1:8080/api/birthday', { headers })
-            .subscribe({
-                next: (res) => {
-                    this.dados = res;
-                },
-                error: () => {
-                    localStorage.removeItem('token');
-                    this.router.navigate(['/auth']);
-                },
-            });
+    submitBirthadayContent(event: Event) {
+        event.preventDefault();
+        this.birthdayService.postBirthday(this.form);
     }
 }
